6839ae40fe2d7b40a00612c6821da05277d43055
Omar Ouda <39740521+OmarOuda1@users.noreply.github.com>
2025-10-30 12:23:38 +0300

    Run the task on APP_CORE

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@ void Motors_Task(void*);
 TaskHandle_t motors_task;
 QueueHandle_t motors_queue;
 
-#define QUEUE_LENGTH 5
+#define QUEUE_SIZE 5
 
 struct point
 {
@@ -37,14 +37,15 @@ L293D leftmotor(IN3_B,IN4_B,EN_B,1);
 void setup() {
 
 // ======= Motors ======= //
-    motors_queue = xQueueCreate( QUEUE_LENGTH , sizeof(point) );
-    xTaskCreate(Motors_Task,      // Function name
-                 "Motors_Task",   // Task name
-                 1024,            // Stack size in words (Word = 4 bytes)
-                 NULL,            // Task parameters
-                 2,               // Task priority (From 0 to 24)
-                 &motors_task     // Pointer to task handle
-                );
+    motors_queue = xQueueCreate( QUEUE_SIZE , sizeof(point) );
+    xTaskCreatePinnedToCore(Motors_Task,     // Function name
+                            "Motors_Task",   // Task name
+                            1024,            // Stack size in words (Word = 4 bytes)
+                            NULL,            // Task parameters
+                            2,               // Task priority (From 0 to 24)
+                            &motors_task ,   // Pointer to task handle
+                            APP_CPU_NUM      // Core number
+                            );
   
 }
 
@@ -57,7 +58,7 @@ void Motors_Task(void* pvParameters) {
     // Create the code that takes values from a queue and execute those values on the motors
     // The number of motors is 4 but each two is wired together so the number of motors you can control is 2
     // The values are in the from of x and y values use these values to determine the speed of the two motors
-    // Use a library to manage the motors dsend any signals yourself just use the library
+    // Use a library to manage the motors do not send any signals yourself just use the library
     // You objective is to use the x and y values to change the speed of the motors.
 
     // Examples
